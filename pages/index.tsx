6c72439ce4ac8fc9a31f7b48dd53d1353ff86196
Tevfik <tevfikhoglu@gmail.com>
2022-05-01 10:43:53 +0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home = () => {
+  return <div>Home</div>
+}
+
+export default Home
